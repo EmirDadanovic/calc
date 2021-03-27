eb323b4c2c9ea1c06af342a93de096704d7d5126
EmirDadanovic <56722228+EmirDadanovic@users.noreply.github.com>
2021-03-27 17:36:43 +0100

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+       
+    
+        
+
+        
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Unesite prvi broj: ");
+                double broj1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Unesite Matematicku Operaciju ");
+                string matop = Console.ReadLine();
+                Console.WriteLine("Unesite drugi broj");
+                double broj2 = Convert.ToDouble(Console.ReadLine());
+
+
+                if (matop == "+")
+                {
+                    Console.WriteLine(broj1 + broj2);
+
+                }
+                else if (matop == "/")
+                {
+                    Console.Write(broj1 / broj2);
+                }
+                else if (matop == "*")
+                {
+                    Console.WriteLine(broj1 * broj2);
+
+                }
+                else if (matop == "-")
+                {
+                    Console.WriteLine(broj1 - broj2);
+                }
+                else
+                {
+                    Console.WriteLine("niste unejeli validnu matematicku operaciju");
+                }
+
+                Console.ReadLine();
+            }
+
+
+
+        }
+    }
